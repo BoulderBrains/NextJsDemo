946de663e5a4ec6683fa1e9a4c6cacda4b5de0ba
Joshua <joshuadstarkey@gmail.com>
2022-02-17 10:19:52 -0700

    feat: cleaning up componets with new global styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70ade3374a01b0f0"; }
+/******/ 		__webpack_require__.h = function() { return "9212af38a06e95b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
